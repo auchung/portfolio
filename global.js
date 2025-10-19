b0aa0f0fe04445505dbac0e226008a55b8663331
auchung <auchung@ucsd.edu>
2025-10-18 21:15:53 -0700

    lab4 step3

--- global.js
+++ global.js
@@ -148,3 +148,7 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     containerElement.appendChild(article);
   }
 }
+
+export async function fetchGitHubData(username) {
+    return await fetchJSON(`https://api.github.com/users/${username}`);
+  }
--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 import { fetchJSON, renderProjects } from './global.js';
+import { fetchGitHubData } from './global.js';
 
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
+
+const githubData = await fetchGitHubData('auchung'); 
+const profileStats = document.querySelector('#profile-stats');
+if (profileStats && githubData) {
+        profileStats.innerHTML = `
+          <h2>GitHub Stats</h2>
+          <dl>
+            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+            <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+            <dt>Followers:</dt><dd>${githubData.followers}</dd>
+            <dt>Following:</dt><dd>${githubData.following}</dd>
+          </dl>
+        `;
+}
